94f7330475c059d7001fee10067cd9abe35d28d5
dean24-up <dean24@up.edu>
2024-02-15 11:02:41 -0800

    first draw - Part B

--- app/src/main/java/com/example/deanfacemaker1/Face.java
+++ app/src/main/java/com/example/deanfacemaker1/Face.java
@@ -8,22 +8,41 @@
 
 package com.example.deanfacemaker1;
 
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
 
 import java.util.Random;
 
-public class Face {
+/*
+    External Citation
+    Date: 15 February 2024
+    Problem: Didn't remember how to set up a class to paint
+    Resource: SurfaceViewDrawingSourceCode on Moodle
+    Solution: referenced how classes were set up
+ */
+public class Face extends SurfaceView {
     private int skinColor;
     private int eyeColor;
     private int hairColor;
     private int hairStyle;
 
+    private Paint facePaint = new Paint();
+
     /**
      * Constructor
      */
-    public Face() {
+    public Face(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setWillNotDraw(false);
         randomize();
+        facePaint.setColor(skinColor); //pink as a placeholder
+        facePaint.setStyle(Paint.Style.FILL);
+        setBackgroundColor(Color.WHITE);
+
     }
 
     /**
@@ -33,7 +52,9 @@ public class Face {
     public void onDraw(Canvas canvas) {
         //TOFIX add in part b
         //draw skin/face
+        canvas.drawCircle(600.0f, 550.0f, 350.0f, facePaint);
         //call draw eye and draw hair
+
     }
 
     /**
